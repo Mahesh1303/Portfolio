865921027311f2f3972f9b4273a10ad202675fc8
Mahesh <maheshddivate@gmail.com>
2024-11-20 21:49:57 +0530

    Made Some Changes

--- src/pages/Projects/Project.page.tsx
+++ src/pages/Projects/Project.page.tsx
@@ -72,13 +72,14 @@ const MyProjects: React.FC = () => {
       <h2 className="text-5xl font-extrabold text-amber-500 mb-4 text-center">My Projects</h2>
       <div className="flex flex-col items-center justify-start min-h-screen p-8 bg-gray-900 shadow-lg rounded-lg mx-4">
 
-        <div className="w-full max-w-6xl rounded-lg p-8 mb-8 bg-gray-900"> {/* Increased container width */}
+        <div className="w-full rounded-lg p-8 mb-8 bg-gray-900">
           <div className="mb-4">
             <h2 className="text-3xl font-semibold text-blue-600"></h2>
             <p className="text-gray-600 mt-2">Explore the following projects I built </p>
           </div>
 
-          <div className="flex overflow-x-auto space-x-8 py-4 snap-x snap-mandatory">
+          {/* Increase width of the horizontal scroll container */}
+          <div className="flex overflow-x-auto space-x-8 py-4 snap-x snap-mandatory w-[120%]"> {/* Here I increased the width */}
             {projectData.map((project, index) => (
               <div 
                 key={index} 
